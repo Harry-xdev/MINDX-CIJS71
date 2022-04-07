687d1c96a8205f256dbcbb2bd13168f26f95ee88
harry-xdev <harry.xdev@gmail.com>
2022-04-07 23:54:54 +0700

    name

---
+++ session-3/src/App.js
@@ -0,0 +1,20 @@
+import { InputSection } from './components/InputBar/input';
+import './App.css';
+import { TaskBar } from './components/TaskBar/taskbar';
+import { StatusBar } from './components/StatusBar/statusbar';
+
+function App(props) {
+  return (
+    <div className="App">
+      <InputSection />
+      <TaskBar task='Clean up bedroom' />
+      <TaskBar task='Buy some milk' />
+      <TaskBar task='Jogging' />
+      <TaskBar task='Lean React' />
+      <TaskBar task='Doing Exercises' />
+      <StatusBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ session-3/src/components/InputBar/input.js
@@ -0,0 +1,9 @@
+import './input.css'
+
+export const InputSection = () => {
+    return (
+        <div className='inputDiv'>
+            <input className='input' placeholder='Enter your task here...'></input>
+        </div>
+    )
+}
