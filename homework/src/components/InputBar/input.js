3c58240889f7bf5ca2c251c35373e89c442d0249
harry-xdev <harry.xdev@gmail.com>
2022-04-08 00:11:26 +0700

    linhtinh

--- homework/src/components/InputBar/input.js
+++
@@ -1,9 +0,0 @@
-import './input.css'
-
-export const InputSection = () => {
-    return (
-        <div className='inputDiv'>
-            <input className='input' placeHolder='Enter your task here...'></input>
-        </div>
-    )
-}
--- homework/src/components/TaskBar/taskbar.js
+++
@@ -1,9 +0,0 @@
-import './taskbar.css'
-export const TaskBar = (props) => {
-    return (
-        <div className='taskBar'>
-            <div className="circle"></div>
-            {props.task}
-        </div>
-    )
-}
