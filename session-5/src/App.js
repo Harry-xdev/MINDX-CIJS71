d1054ff7ec9152a4c3d96e25006b66c8e7b11179
Harry-xdev <harry.xdev@gmail.com>
2022-04-08 16:02:47 +0700

    staffic

---
+++ session-5/src/App.js
@@ -0,0 +1,12 @@
+import { Todolist } from './components/Todolist/task';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Todolist />
+    </div>
+  );
+}
+
+export default App;
