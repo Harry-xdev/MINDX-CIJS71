6670b2d02b04aca6963b3d557945b7076a34eeaf
harry-xdev <harry.xdev@gmail.com>
2022-04-16 23:23:34 +0700

    hw

---
+++ session-8/src/components/MainPage/main.js
@@ -0,0 +1,42 @@
+import './main.css'
+export const MainPage = () => {
+
+    return (
+        <div>
+            <div className="paragraph">
+                <p>
+                    Surface Việt - Phân phối sản phẩm Surface Pro 8, Surface Laptop Studio, phụ kiện Microsoft Surface dành cho người dùng văn phòng đang triển khai Event Noel năm mới 2022, giảm giá lên tới 2 triệu VNĐ, tặng kèm cả chuột Surface Mobile Mouse.
+
+                    Surface Việt luôn là địa điểm tin tưởng khi quý khách hàng cần tìm sản phẩm liên quan tới Microsoft, để cung cấp việc trải nghiệm với những sản phẩm Microsoft một cách tốt nhất.
+
+                    Hiện tại, Surface Việt đang phân phối Microsoft Surface tất cả các sản phẩm máy tính Surface và các phụ kiện Microsoft phục vụ công việc văn phòng mới nhất như: Surface Pro 8, Surface Laptop Studio, Surface Go 3… Cụ thể như thế nào, xin mời anh em bớt chút thời gian “ngó nghiêng” xem hệ sinh thái của Microsoft ở thời điểm hiện tại gồm có những gì nhé!
+
+                    Surface Pro 8 - máy tính 2 trong 1 tốt nhất cho công việc văn phòng
+                    Surface Pro 8 là mẫu máy tính đặc biệt, vừa là laptop, vừa là máy tính bảng. Với kiểu dáng của 1 chiếc máy tính bảng rất nhỏ gọn, nhẹ, chỉ hơn 800 gram một chút. Khi gắn bàn phím, máy sẽ trở thành 1 chiếc laptop chạy hệ điều hành Windows 11 mới nhất của Microsoft.
+
+                    Màn hình 13 inch, độ phân giải cực khủng 2880x1920 pixel, cho mật độ điểm ảnh đạt tới 267ppi, giúp hình ảnh hiển thị trên máy cực kỳ đẹp, sắc nét. Màn hình cũng có cảm ứng 10 điểm chạm, đi kèm đó là tần số quét lên tới 120Hz giúp thao tác vuốt chạm, viết, vẽ, note ghi chú lên màn hình máy cực kỳ mượt.
+                    Surface Go 3 - mẫu máy tính tốt nhất cho học sinh, sinh viên
+                    Surface Go 3 là mẫu máy tính có thiết kế tương tự như Pro 8 và hướng tới người dùng là các bạn học sinh, sinh viên. Đây là món quà tặng ý nghĩa của các bậc phụ huynh dành tặng cho con cái trong thời buổi học hành online đã trở thành xu hướng trong tương lai. Các bạn nhỏ sẽ sớm tiếp cận với laptop chạy hệ điều hành Windows 11, đi kèm bộ công cụ Word, Excel, Powerpoint ngay từ khi còn học tập trên ghế nhà trường.
+
+                    Khi cần giải trí, chỉ cần tháo rời bàn phím, máy cũng sẽ trở thành máy tính bảng, với màn hình 10.5 inch, độ phân giải full HD 1920x1280 hiển thị sắc nét. Máy cũng có cảm ứng 10 điểm chạm, thuận tiện cho việc thao tác sử dụng máy. Trọng lượng của máy cũng rất nhẹ, chỉ hơn 0,5kg, rất dễ dàng bỏ balo, cặp sách khi mang đi học.
+
+                    Với mức giá 13.390.000đ tại Surface Việt, Surface Go 3 sẽ là người bạn tuyệt vời, đồng hành cùng các bạn nhỏ trong suốt quãng thời gian học tập phổ thông.
+
+                    Link mua hàng
+
+                    Surface Laptop Studio - mẫu máy tính dành cho người dùng làm việc chuyên nghiệp
+                    Surface Laptop Studio là mẫu máy tính mới nhất của Microsoft vừa mới được ra mắt. Đây là mẫu máy tính hướng tới người dùng làm việc chuyên nghiệp. Cụ thể là những người dùng cần một chiếc máy tính xách tay để làm các công việc nặng về công nghệ nhưng vẫn cần tính di động nhỏ gọn và thiết kế đẹp, thời trang.
+
+                    Surface Laptop Studio được trang bị màn hình 14.4” 2400x1600, tỷ lệ màn hình 3:2 tối ưu cho công việc, tần số quét 120Hz và có cảm ứng 10 điểm chạm giúp thao tác viết, vẽ lên màn hình rất mượt. Do là chiếc máy hướng tới làm việc “nặng” nên máy cũng được tích hợp cấu hình khủng với Intel I5 11300H hoặc I7 11370H, cả 2 đều là CPU dòng H với hiệu năng cao. Mức RAM 16 hoặc 32GB LPDDR4x. Card đồ họa Iris Xe cho bản i5 và Nvidia GeForce RTX 3050Ti cho bản i7. Bộ nhớ trong SSD 256GB, 512GB, 1TB, 2TB.
+
+                    Với thông số cấu hình trên giúp người dùng có thể tự tin xử lý mọi tác vụ công việc liên quan tới sáng tạo nội dung ở thời điểm hiện tại như: code IT lập trình, thiết kế đồ họa Adobe Ps, AI, Pr, AE …
+
+                    Điểm đặc biệt của Surface Laptop Studio là phần màn hình của máy có thể di chuyển, gập ở nhiều tư thế khác nhau, giúp thuận tiện cho việc thao tác vẽ thiết kế, trình chiếu hình ảnh nội dung cho đối tác, khách hàng dễ dàng.
+                </p>
+            </div>
+            <button className="btn">
+                Back to top
+            </button>
+        </div>
+    )
+}
